8c32da518ac0ee104d73de58f5a4718780a21f34
Robert Boros <robbie13.boros@gmail.com>
2022-04-07 10:18:42 +0200

    add localstorage mutex

--- src/guardian/helper.ts
+++ src/guardian/helper.ts
@@ -7,4 +7,55 @@ const hash = async (eventData: EventData): Promise<string> => {
     return new TextDecoder().decode(digest)
 }
 
-export default hash
+const localStorageLockTimeout = 10
+
+const setLocalStorage = (key: string, value: Record<string, any>, ttl = -1) => {
+    if (!localStorage.getItem(key+'-mutex')) {
+        localStorage.setItem(key+'-mutex', 'true')
+    } else {
+        setTimeout(() => ({}), localStorageLockTimeout)
+        setLocalStorage(key, value, ttl)
+    }
+    localStorage.setItem(key, JSON.stringify({expr: ttl + +(new Date()), value: JSON.stringify(value)}))
+    localStorage.removeItem(key+'-mutex')
+}
+
+const updateLocalStorage = (key: string, update: (v: string) => any, ttl = -1) => {
+    if (!localStorage.getItem(key+'-mutex')) {
+        localStorage.setItem(key+'-mutex', 'true')
+    } else {
+        setTimeout(() => ({}), localStorageLockTimeout)
+        updateLocalStorage(key, update, ttl)
+    }
+    const item = localStorage.getItem(key);
+    if (!item) {
+        localStorage.removeItem(key+'-mutex')
+        return
+    }
+    localStorage.setItem(key, JSON.stringify({expr: ttl + +(new Date()), value: JSON.stringify(update(item))}))
+    localStorage.removeItem(key+'-mutex')
+}
+
+const getLocalStorage = (key: string): string | null => {
+    if (!localStorage.getItem(key+'-mutex')) {
+        localStorage.setItem(key+'-mutex', 'true')
+    } else {
+        setTimeout(() => ({}), localStorageLockTimeout)
+        return getLocalStorage(key)
+    }
+    const item = localStorage.getItem(key);
+    if (!item) {
+        localStorage.removeItem(key+'-mutex')
+        return null
+    }
+    const parsed = JSON.parse(item) as { expr: number; value: string };
+    if (parsed.expr > +(new Date())) {
+        localStorage.removeItem(key)
+        localStorage.removeItem(key+'-mutex')
+        return null
+    }
+    localStorage.removeItem(key+'-mutex')
+    return parsed.value
+}
+
+export {hash, setLocalStorage, getLocalStorage}
