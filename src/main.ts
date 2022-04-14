1cedadbfa58a38c97c740f08799ee553d4fb4fe1
Robert Boros <robbie13.boros@gmail.com>
2022-04-14 14:31:51 +0200

    remove commented lines

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
 import { Guardian } from './guardian/guardian';
 const endpoint = 'http://localhost:8001'
 new Guardian(endpoint).process()
-
-
-const btn = document.createElement('button')
-btn.innerText = 'click me'
-btn.onclick = () => {
-    Guardian.showResult()
-}
-document.body.appendChild(btn)
