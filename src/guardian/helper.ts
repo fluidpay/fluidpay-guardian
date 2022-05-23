ccf9e4663dfcab5bcce63789e187a3d2b2993253
Jeff Rooney <jeff@fluidpay.com>
2022-05-23 15:39:18 -0500

    ran prettier

--- src/guardian/helper.ts
+++ src/guardian/helper.ts
@@ -57,13 +57,13 @@ const teeFunc = (func: () => void): (() => void) => {
 };
 
 function debounce<F extends (...params: any[]) => void>(fn: F, delay: number) {
-    let timeoutID: number | null = null
+    let timeoutID: number | null = null;
     return function (this: any, ...args: any[]) {
-      if (timeoutID !== null) {
-        clearTimeout(timeoutID)
-      }
-      timeoutID = window.setTimeout(() => fn.apply(this, args), delay)
-    } as F
-  }
+        if (timeoutID !== null) {
+            clearTimeout(timeoutID);
+        }
+        timeoutID = window.setTimeout(() => fn.apply(this, args), delay);
+    } as F;
+}
 
 export { hash, connectDB, teeFunc, debounce };
